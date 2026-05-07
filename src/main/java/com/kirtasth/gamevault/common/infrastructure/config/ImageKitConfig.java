263179e9bbb156311b7315b89ea21c4b6da343bf
Kirtasth <davidsotoarb@gmail.com>
2026-05-07 09:51:09 +0200

    feat(infra): add ImageKit configuration for production profile
    - Introduced `ImageKitConfig` to initialize `ImageKitClient` with private key and URL endpoint for the "pro" profile.
    - Updated `application.yml` to include `imagekit.private-key` and `imagekit.url-endpoint` configuration properties.

--- src/main/java/com/kirtasth/gamevault/common/infrastructure/config/ImageKitConfig.java
+++ src/main/java/com/kirtasth/gamevault/common/infrastructure/config/ImageKitConfig.java
@@ -2,6 +2,7 @@ package com.kirtasth.gamevault.common.infrastructure.config;
 
 import io.imagekit.client.ImageKitClient;
 import io.imagekit.client.okhttp.ImageKitOkHttpClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -10,8 +11,17 @@ import org.springframework.context.annotation.Profile;
 @Profile("pro")
 public class ImageKitConfig {
 
+    @Value("${imagekit.private-key}")
+    private String privateKey;
+
+    @Value("${imagekit.url-endpoint}")
+    private String urlEndpoint;
+
     @Bean
     public ImageKitClient imageKitClient() {
-        return ImageKitOkHttpClient.fromEnv();
+        return ImageKitOkHttpClient.builder()
+                .privateKey(privateKey)
+                .baseUrl(urlEndpoint)
+                .build();
     }
 }
